0f41ee4d93a3ea75357d5f5209dc7ccc53f0cc49
Lucas Santos <lucas.parkl@outlook.com>
2022-11-05 23:07:22 -0300

    WIP: React - desenvolvendo em JavaScript: - controlar inputs com eventos de OnChange e a propriedade value - gerenciando estados de um componentes utilizando useStates que são controlados pela React - passando uma função via props para baixo (componente filho)

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -2,24 +2,63 @@ import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
+import { useState } from "react";
 
-const times = ["UX", "Programação", "Front end", "Batatinha Legal"]
+const Formulario = (props) => {
+
+    //Ao criar um useState, é devolvido 2 itens, a variavel e o método de set dessa variavel
+    const [nome, setNome] = useState("");
+    const [cargo, setCargo] = useState("");
+    const [imagem, setImagem] = useState("");
+    const [time, setTime] = useState("");
+
+    const times = ["UX", "Programação", "Front end", "Batatinha Legal"]
 
 const aoSalvar = (evento) => {
   evento.preventDefault()
-  console.log("Form foi submetido")
+  //console.log("Form foi submetido => Nome:" + nome + " Cargo: " + cargo + " Imagem: " + imagem + "Time: " + time) 
+  props.aoColaboradorCadastrado({
+    nome: nome, 
+    cargo: cargo, 
+    imagem: imagem,
+    time: time 
+  })
 }
 
-const Formulario = () => {
+
   return (
     <section className="formulario">
-      <form onSubmit={aoSalvar }>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
         {/* Passando parametro para o componente React */}
-        <CampoTexto  obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto obrigatorio={true} label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} itens={times} label="times"></ListaSuspensa>
+        <CampoTexto  
+        obrigatorio={true} 
+        label="Nome" 
+        placeholder="Digite seu nome" 
+        valor={nome}
+        setValor={valor => setNome(valor)}
+        />
+        <CampoTexto 
+        obrigatorio={true} 
+        label="Cargo" 
+        placeholder="Digite seu cargo" 
+        valor={cargo}
+        setValor={valor => setCargo(valor)}
+        />
+        <CampoTexto 
+        obrigatorio={true} 
+        label="Imagem" 
+        placeholder="Informe o endereço da imagem" 
+        valor={imagem}
+        setValor={valor => setImagem(valor)}
+        />
+        <ListaSuspensa 
+        obrigatorio={true} 
+        itens={times} 
+        label="times"
+        valor={time}
+        setValor={valor => setTime(valor)}
+        />
         <Botao>
           Criar Card
         </Botao>
--- organo/src/components/ListaSuspensa/index.js
+++ organo/src/components/ListaSuspensa/index.js
@@ -1,10 +1,15 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
+
+    const aoAlterado = (evento) => {
+        props.setValor(evento.target.value);
+      };
+    
     return (
         <div className='listaSuspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
+            <select onChange={aoAlterado} required={props.obrigatorio} value={props.valor}>
                 {/* for each onde para cada item é criado um item de option */}
                 {/* adicionando chave para o react saber quando precisar qual item gerar*/}
                 {props.itens.map(item => <option key={item}> {item}</option>)}
